cb1227c24c296d2920565f1e5e41d2b7f5135d9d
Pedro Fernandes [1060503] <1060503@isep.ipp.pt>
2024-04-13 23:39:56 +0100

    DomainToXMLTest implementation

--- src/main/scala/pj/xml/DomainToXML.scala
+++ src/main/scala/pj/xml/DomainToXML.scala
@@ -17,20 +17,19 @@ object DomainToXML:
       <error xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="../../scheduleError.xsd"
              message={error.toString}/>
   private def vivaListToXML(schedule: OutputSchedule): Elem =
-    <schedule
-            xsi:noNamespaceSchemaLocation="../../schedule.xsd" totalPreference={schedule.totalPreference.toString} xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        {
-          schedule.vivasSchedule.map( viva =>
-            <viva student={viva.student.to} title={viva.title.to} start={viva.start.to} end={viva.end.to} preference={viva.preference.toString}>
-              {
-              viva.jury.flatMap {
-                case Role.President(role) => Some(<president name={role.name.to}/>)
-                case Role.Advisor(role) => Some(<advisor name={role.name.to}/>)
-                case Role.CoAdvisor(role) => Some(<coadvisor name={role.name.to}/>)
-                case Role.Supervisor(role) => Some(<supervisor name={role.name.to}/>)
-              }
-              }
-            </viva>
-          )
-        }
-    </schedule>
+    <schedule xsi:noNamespaceSchemaLocation="../../schedule.xsd" totalPreference={schedule.totalPreference.toString} xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      {
+        schedule.vivasSchedule.map( viva =>
+          <viva student={viva.student.to} title={viva.title.to} start={viva.start.to} end={viva.end.to} preference={viva.preference.toString}>
+            {
+            viva.jury.flatMap {
+              case Role.President(role) => Some(<president name={role.name.to}/>)
+              case Role.Advisor(role) => Some(<advisor name={role.name.to}/>)
+              case Role.CoAdvisor(role) => Some(<coadvisor name={role.name.to}/>)
+              case Role.Supervisor(role) => Some(<supervisor name={role.name.to}/>)
+            }
+            }
+          </viva>
+        )
+      }
+  </schedule>
---
+++ src/test/scala/pj/domain/schedule/DomainToXMLTest.scala
@@ -0,0 +1,71 @@
+package pj.domain.schedule
+
+import scala.language.adhocExtensions
+import org.scalatest.funsuite.AnyFunSuite
+import pj.domain.Role.*
+import pj.domain.*
+import pj.domain.SimpleTypes.*
+import pj.domain.{ExternalId, Role, VivaSchedule}
+import pj.io.FileIO
+import pj.xml.DomainToXML
+
+import scala.xml.{Elem, PrettyPrinter, Utility}
+
+class DomainToXMLTest extends AnyFunSuite:
+  test("DomainToXML"):  
+    for
+      date1         <- DateTime.from("2022-01-01T09:00:00")
+      date2         <- DateTime.from("2022-01-01T10:00:00")
+      date3         <- DateTime.from("2022-01-02T09:00:00")
+      date4         <- DateTime.from("2022-01-02T10:00:00")
+      date5         <- DateTime.from("2022-01-03T09:00:00")
+      date6         <- DateTime.from("2022-01-03T10:00:00")
+      pref1         <- Preference.from(1)
+      pref2         <- Preference.from(2)
+      pref3         <- Preference.from(3)
+      availability1 = Availability(date1, date2, pref1)
+      availability2 = Availability(date3, date4, pref2)
+      availability3 = Availability(date5, date6, pref3)
+      tid1          <- TeacherId.from("T001")
+      nameT1        <- Name.from("Teacher 1")
+      tid2          <- TeacherId.from("T002")
+      nameT2        <- Name.from("Teacher 2")
+      externalId    <- ExternalId.from("E001")
+      externalName  <- Name.from("External 1")
+      teacher1 = Teacher(tid1, nameT1, List(availability1, availability2))
+      teacher2 = Teacher(tid2, nameT2, List(availability2, availability3))
+      external1 = External(externalId, externalName, List(availability1, availability3))
+      jury = List(
+        Role.President(teacher1),
+        Role.Advisor(teacher2),
+        Role.Supervisor(external1)
+      )
+      student       <- Student.from("John")
+      title         <- Title.from("Title")
+      start         <- DateTime.from("2022-01-01T09:00:00")
+      end           <- DateTime.from("2022-01-01T10:00:00")
+      preference    <- Preference.from(4)
+      totalPref     <- Preference.from(4)
+    yield
+      val vivaSchedule = VivaSchedule(student, title, jury, start, end, preference)
+      val outputSchedule = OutputSchedule(List(vivaSchedule), totalPref)
+      val xml = DomainToXML.generateOutputXML(Right(outputSchedule))
+      // Create a PrettyPrinter
+      val printer = new PrettyPrinter(120, 4)
+      // Use the PrettyPrinter to format the XML
+      val formattedXml: String = printer.format(xml)
+      println(formattedXml)
+      def expected: Elem =
+        <schedule xsi:noNamespaceSchemaLocation="../../schedule.xsd" totalPreference="4"
+                  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+          <viva student="John" title="Title" start="2022-01-01T09:00" end="2022-01-01T10:00" preference="4">
+            <president name="Teacher 1"/>
+            <advisor name="Teacher 2"/>
+            <supervisor name="External 1"/>
+          </viva>
+        </schedule>
+      val expectedXml: String = printer.format(expected)
+      assert(formattedXml != null)
+      assert(formattedXml === expectedXml)
+
+
