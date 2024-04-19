278ef9419980a02b79a8cd5949d0168566b3cf00
Pedro Fernandes [1060503] <1060503@isep.ipp.pt>
2024-04-19 22:03:10 +0100

    fix unit tests

--- src/test/scala/pj/domain/schedule/ScheduleMS01Test.scala
+++ src/test/scala/pj/domain/schedule/ScheduleMS01Test.scala
@@ -15,97 +15,46 @@ class ScheduleMS01Test extends AnyFunSuite, ScheduleMS01Behaviours:
       xml <- load("files/test/ms01/simple01_in.xml")
       result <- ScheduleMS01.create(xml)
       expected <- load("files/test/ms01/simple01_out.xml")
-    yield
-      // Create a PrettyPrinter
-      val printer = new PrettyPrinter(120, 4)
-      // Use the PrettyPrinter to format the XML
-      val resultXml: String = printer.format(result)
-      val expectedXml: String = printer.format(expected)
-      assert(resultXml === expectedXml)
-      assert(Utility.trim(result) == Utility.trim(expected))
+    yield assert(Utility.trim(result) == Utility.trim(expected))
 
   test("Valid agenda 01"):
       for
         xml <- load("files/assessment/ms01/valid_agenda_01_in.xml")
         result <- ScheduleMS01.create(xml)
         expected <- load("files/assessment/ms01/valid_agenda_01_out.xml")
-      yield
-        // Create a PrettyPrinter
-        val printer = new PrettyPrinter(120, 4)
-        // Use the PrettyPrinter to format the XML
-        val resultXml: String = printer.format(result)
-        val expectedXml: String = printer.format(expected)
-        assert(resultXml === expectedXml)
-        assert(Utility.trim(result) == Utility.trim(expected))
+      yield assert(Utility.trim(result) == Utility.trim(expected))
 
   test("Invalid agenda 01"):
     for
       xml <- load("files/assessment/ms01/invalid_agenda_01_in.xml")
       result <- ScheduleMS01.create(xml)
       expected <- load("files/assessment/ms01/invalid_agenda_01_outError.xml")
-    yield
-      // Create a PrettyPrinter
-      val printer = new PrettyPrinter(120, 4)
-      // Use the PrettyPrinter to format the XML
-      val resultXml: String = printer.format(result)
-      val expectedXml: String = printer.format(expected)
-      assert(resultXml === expectedXml)
-      assert(Utility.trim(result) == Utility.trim(expected))
+    yield assert(Utility.trim(result) == Utility.trim(expected))
 
   test("Simple 02"):
     for
       xml <- load("files/test/ms01/simple02_in.xml")
       result <- ScheduleMS01.create(xml)
       expected <- load("files/test/ms01/simple02_out.xml")
-    yield
-      // Create a PrettyPrinter
-      val printer = new PrettyPrinter(120, 4)
-      // Use the PrettyPrinter to format the XML
-      val resultXml: String = printer.format(result)
-      val expectedXml: String = printer.format(expected)
-      assert(resultXml === expectedXml)
-      assert(Utility.trim(result) == Utility.trim(expected))
+    yield assert(Utility.trim(result) == Utility.trim(expected))
 
   test("Simple 03"):
     for
       xml <- load("files/test/ms01/simple03_in.xml")
       result <- ScheduleMS01.create(xml)
       expected <- load("files/test/ms01/simple03_outError.xml")
-    yield
-      // Create a PrettyPrinter
-      val printer = new PrettyPrinter(120, 4)
-      // Use the PrettyPrinter to format the XML
-      val resultXml: String = printer.format(result)
-      val expectedXml: String = printer.format(expected)
-      assert(resultXml === expectedXml)
-      assert(Utility.trim(result) == Utility.trim(expected))
-
-
+    yield assert(Utility.trim(result) == Utility.trim(expected))
 
   test("Simple 04"):
     for
       xml <- load("files/test/ms01/simple04_in.xml")
       result <- ScheduleMS01.create(xml)
       expected <- load("files/test/ms01/simple04_outError.xml")
-    yield
-      // Create a PrettyPrinter
-      val printer = new PrettyPrinter(120, 4)
-      // Use the PrettyPrinter to format the XML
-      val resultXml: String = printer.format(result)
-      val expectedXml: String = printer.format(expected)
-      assert(resultXml === expectedXml)
-      assert(Utility.trim(result) == Utility.trim(expected))
+    yield assert(Utility.trim(result) == Utility.trim(expected))
 
   test("Simple 05"):
     for
       xml <- load("files/test/ms01/simple05_in.xml")
       result <- ScheduleMS01.create(xml)
       expected <- load("files/test/ms01/simple05_out.xml")
-    yield
-      // Create a PrettyPrinter
-      val printer = new PrettyPrinter(120, 4)
-      // Use the PrettyPrinter to format the XML
-      val resultXml: String = printer.format(result)
-      val expectedXml: String = printer.format(expected)
-      assert(resultXml === expectedXml)
-      assert(Utility.trim(result) == Utility.trim(expected))
+    yield assert(Utility.trim(result) == Utility.trim(expected))
