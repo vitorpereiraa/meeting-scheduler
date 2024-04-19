642143afeb88f0531bc4fef9c4234b76555a3b4d
Alexandra Leite [1170541] <1170541@isep.ipp.pt>
2024-04-19 12:42:14 +0100

    Added new tests with new XML files

--- src/test/scala/pj/domain/schedule/ScheduleMS01Test.scala
+++ src/test/scala/pj/domain/schedule/ScheduleMS01Test.scala
@@ -5,7 +5,10 @@ import pj.io.FileIO.load
 
 import scala.xml.*
 
-class ScheduleMS01Test extends AnyFunSuite:
+class ScheduleMS01Test extends AnyFunSuite, ScheduleMS01Behaviours:
+
+  val PATH = "files/test/ms01" // Schedule file path
+  performTests(ScheduleMS01.create, "Milestone 1")
 
   test("Simple 01"):
     for
@@ -40,6 +43,34 @@ class ScheduleMS01Test extends AnyFunSuite:
       xml <- load("files/assessment/ms01/invalid_agenda_01_in.xml")
       result <- ScheduleMS01.create(xml)
       expected <- load("files/assessment/ms01/invalid_agenda_01_outError.xml")
+    yield
+      // Create a PrettyPrinter
+      val printer = new PrettyPrinter(120, 4)
+      // Use the PrettyPrinter to format the XML
+      val resultXml: String = printer.format(result)
+      val expectedXml: String = printer.format(expected)
+      assert(resultXml === expectedXml)
+      assert(Utility.trim(result) == Utility.trim(expected))
+
+  test("Simple 02"):
+    for
+      xml <- load("files/test/ms01/simple02_in.xml")
+      result <- ScheduleMS01.create(xml)
+      expected <- load("files/test/ms01/simple02_out.xml")
+    yield
+      // Create a PrettyPrinter
+      val printer = new PrettyPrinter(120, 4)
+      // Use the PrettyPrinter to format the XML
+      val resultXml: String = printer.format(result)
+      val expectedXml: String = printer.format(expected)
+      assert(resultXml === expectedXml)
+      assert(Utility.trim(result) == Utility.trim(expected))
+
+  test("Simple 03"):
+    for
+      xml <- load("files/test/ms01/simple03_in.xml")
+      result <- ScheduleMS01.create(xml)
+      expected <- load("files/test/ms01/simple03_outError.xml")
     yield
       // Create a PrettyPrinter
       val printer = new PrettyPrinter(120, 4)
