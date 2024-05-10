a74f76242f3a2fdced859388313ff101a152a85a
Vitor Pereira <1191244@isep.ipp.pt>
2024-05-10 15:23:14 +0100

    added generator for datetime simple type

--- src/test/scala/pj/property/Generators.scala
+++ src/test/scala/pj/property/Generators.scala
@@ -18,10 +18,16 @@ object Generators extends Properties("Generators"):
   val MAX_HOUR = 23
   val MIN_MINUTE = 0
   val MAX_MINUTE = 59
+  val MIN_SECOND = 0
+  val MAX_SECOND = 59
   val HOUR_MINUTE_LENGTH = 2
+  val MIN_YEAR = 1000
+  val MAX_YEAR = 9999
+  val MIN_MONTH = 1
+  val MAX_MONTH = 12
+  val MIN_DAY = 1
 
   // Generators
-
   private def preferenceGen: Gen[Preference] =
     for
       num <- Gen.chooseNum(MIN_PREF_LIMIT, MAX_PREF_LIMIT)
@@ -66,11 +72,30 @@ object Generators extends Properties("Generators"):
       name <- nameGenerator(x => Name.from(x))
     yield name
 
+  private def dateTimeGen: Gen[DateTime] =
+    for
+      year         <- Gen.chooseNum(MIN_YEAR, MAX_YEAR)
+      month        <- Gen.chooseNum(MIN_MONTH, MAX_MONTH)
+      day          <- Gen.chooseNum(MIN_DAY, daysInMonth(year, month))
+      hour         <- Gen.chooseNum(MIN_HOUR, MAX_HOUR)
+      minute       <- Gen.chooseNum(MIN_MINUTE, MAX_MINUTE)
+      seconds      <- Gen.chooseNum(MIN_SECOND, MAX_SECOND)
+      monthStr     = month.toString.reverse.padTo(2, '0').reverse
+      dayStr       = day.toString.reverse.padTo(2, '0').reverse
+      hourStr      = hour.toString.reverse.padTo(2, '0').reverse
+      minuteStr    = minute.toString.reverse.padTo(2, '0').reverse
+      secondsStr   = seconds.toString.reverse.padTo(2, '0').reverse
+      dateTime     <- DateTime.from(s"$year-$monthStr-${dayStr}T$hourStr:$minuteStr:$secondsStr").fold(err => Gen.fail, Gen.const)
+    yield dateTime
+
+  private def daysInMonth(year: Int, month: Int): Int =
+    val febDays = if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) 29 else 28 // feb days depend if it is a leap year
+    val days = Array(31, febDays , 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    days(month - 1)
+
   // TODO: Add more generators
 
-
   // Properties
-
   property("Preference must be between 1 and 5") =
     forAll(preferenceGen)(pref => {
       pref.to >= MIN_PREF_LIMIT && pref.to <= MAX_PREF_LIMIT
@@ -108,4 +133,19 @@ object Generators extends Properties("Generators"):
       name => name.to.nonEmpty
     })
 
-  // TODO: Add more properties
+  property("All date times must have valid dates and times") =
+    forAll(dateTimeGen) { d =>
+      val year = d.toLocalDateTime.getYear
+      val month = d.toLocalDateTime.getMonth.getValue
+      val day = d.toLocalDateTime.getDayOfMonth
+      val hour = d.toLocalDateTime.getHour
+      val minute = d.toLocalDateTime.getMinute
+      val second = d.toLocalDateTime.getSecond
+
+      (year >= MIN_YEAR && year <= MAX_YEAR) &&
+      (month >= MIN_MONTH && month <= MAX_MONTH) &&
+      (day >= MIN_DAY && day <= daysInMonth(year, month)) &&
+      (hour >= MIN_HOUR && hour <= MAX_HOUR) &&
+      (minute >= MIN_MINUTE && minute <= MAX_MINUTE) &&
+      (second >= MIN_SECOND && second <= MAX_SECOND)
+    }
