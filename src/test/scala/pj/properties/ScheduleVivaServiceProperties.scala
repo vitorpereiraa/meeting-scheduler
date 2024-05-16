bda302e6fa14eaf449b0bc16c6df2c68d5c88f25
Vitor Pereira <1191244@isep.ipp.pt>
2024-05-16 17:48:41 +0100

    added resources generator for scheduling

--- src/test/scala/pj/properties/ScheduleVivaServiceProperties.scala
+++ src/test/scala/pj/properties/ScheduleVivaServiceProperties.scala
@@ -6,15 +6,45 @@ import org.scalacheck.Prop.forAll
 import pj.properties.DomainProperties.*
 import pj.properties.SimpleTypesProperties.*
 import pj.domain.*
+import pj.domain.SimpleTypes.Duration
 import pj.domain.scheduleviva.ScheduleVivaService
 
 object ScheduleVivaServiceProperties extends Properties("ScheduleVivaServiceProperties"):
+  
+  val MIN_TEACHERS       = 2
+  val MAX_TEACHERS       = 100
+  val MIN_EXTERNALS      = 1
+  val MAX_EXTERNALS      = 100
 
-  def schedulableVivaGen(resources: List[Resource]): Gen[Viva] =
+  def schedulableTeacherGen(tid: Int): Gen[Teacher] =
+    for
+      id   <- teacherIdGen(tid)
+      name <- nameGen
+    yield Teacher(id, name, Nil)
+
+  def schedulableExternalGen(eid: Int): Gen[External] =
+    for
+      id   <- externalIdGen(eid)
+      name <- nameGen
+    yield External(id, name, Nil)
+
+  def schedulableResourcesGen: Gen[List[Resource]] =
+    for
+      teachersQty  <- Gen.choose(MIN_TEACHERS, MAX_TEACHERS)
+      teachers     <- Gen.sequence[List[Teacher], Teacher]((0 to teachersQty).map(teacherGen))
+      externalsQty <- Gen.choose(MIN_EXTERNALS, MAX_EXTERNALS)
+      externals    <- Gen.sequence[List[External], External]((0 to externalsQty).map(externalGen))
+    yield teachers ::: externals
+
+  def schedulableVivasGen(resources: List[Resource])(duration: Duration): Gen[List[Viva]] =
     ???
 
   def schedulableAgendaGen: Gen[Agenda] =
-    ???
+    for
+      duration  <- durationGen
+      resources <- schedulableResourcesGen
+      vivas     <- schedulableVivasGen(resources)(duration)
+    yield Agenda(duration, vivas, resources)
 
   property("Schedule agenda") =
     forAll(schedulableAgendaGen):
