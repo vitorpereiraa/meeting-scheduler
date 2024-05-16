9c5f97793e5e7afcd1009d2d13ad0d89483814e5
Vitor Pereira <1191244@isep.ipp.pt>
2024-05-16 23:51:58 +0100

    [group work] vivaGenerator brainstorming/debugging

--- src/test/scala/pj/properties/ScheduleVivaServiceProperties.scala
+++ src/test/scala/pj/properties/ScheduleVivaServiceProperties.scala
@@ -7,15 +7,19 @@ import pj.properties.DomainProperties.*
 import pj.properties.SimpleTypesProperties.*
 import pj.domain.*
 import pj.domain.SimpleTypes.Duration
+import pj.domain.resource.ResourceService.getNumOfTeachers
+import pj.domain.role.RoleService.{isAdvisor, isPresident}
 import pj.domain.scheduleviva.ScheduleVivaService
 
 object ScheduleVivaServiceProperties extends Properties("ScheduleVivaServiceProperties"):
 
   val MIN_TEACHERS       = 2
-  val MAX_TEACHERS       = 100
+  val MAX_TEACHERS       = 4
   val MIN_EXTERNALS      = 1
-  val MAX_EXTERNALS      = 100
-  val MAX_VIVAS          = 100
+  val MAX_EXTERNALS      = 4
+  val MAX_VIVAS          = 4
+  val MIN_AVAILABILITIES = 0
+  val MAX_AVAILABILITIES = 4
 
   def schedulableTeacherGen(tid: Int): Gen[Teacher] =
     for
@@ -32,9 +36,9 @@ object ScheduleVivaServiceProperties extends Properties("ScheduleVivaServiceProp
   def schedulableResourcesGen: Gen[List[Resource]] =
     for
       teachersQty  <- Gen.choose(MIN_TEACHERS, MAX_TEACHERS)
-      teachers     <- Gen.sequence[List[Teacher], Teacher]((0 to teachersQty).map(teacherGen))
+      teachers     <- Gen.sequence[List[Teacher], Teacher]((0 to teachersQty).map(schedulableTeacherGen))
       externalsQty <- Gen.choose(MIN_EXTERNALS, MAX_EXTERNALS)
-      externals    <- Gen.sequence[List[External], External]((0 to externalsQty).map(externalGen))
+      externals    <- Gen.sequence[List[External], External]((0 to externalsQty).map(schedulableExternalGen))
     yield teachers ::: externals
 
   def schedulableAvailabilityGen(duration: Duration): Gen[Availability] =
@@ -54,17 +58,24 @@ object ScheduleVivaServiceProperties extends Properties("ScheduleVivaServiceProp
       availabilities    <- Gen.listOfN(availabilitiesQty, availabilityGen)
     yield availabilities.::(availability)
 
-  def updateResourceAvailability(role: Role, availability: Availability, duration: Duration): Gen[Role] =
+  def updateResourceAvailability(resource: Resource, availability: Availability, duration: Duration): Gen[Resource] =
     for
       availabilities <- schedulableAvailabilitiesGen(availability, duration)
-    yield role match
-        case Role.President(Teacher(id, name, _)) => Role.President(Teacher(id, name, availabilities))
-        case Role.Advisor(Teacher(id, name, _))   => Role.Advisor(Teacher(id, name, availabilities))
-        case Role.CoAdvisor(Teacher(id, name, _)) => Role.CoAdvisor(Teacher(id, name, availabilities))
-        case Role.CoAdvisor(External(id, name, _)) => Role.CoAdvisor(External(id, name, availabilities))
-        case Role.Supervisor(External(id, name, _)) => Role.Supervisor(External(id, name, availabilities))
-        case _ => role
+    yield resource match
+      case Teacher(id, name, _) =>  Teacher(id, name, availabilities)
+      case External(id, name, _) => External(id, name, availabilities)
 
+  //  def updateResourceAvailability(role: Role, availability: Availability, duration: Duration): Gen[Role] =
+//    for
+//      availabilities <- schedulableAvailabilitiesGen(availability, duration)
+//    yield role match
+//        case Role.President(Teacher(id, name, _)) => Role.President(Teacher(id, name, availabilities))
+//        case Role.Advisor(Teacher(id, name, _))   => Role.Advisor(Teacher(id, name, availabilities))
+//        case Role.CoAdvisor(Teacher(id, name, _)) => Role.CoAdvisor(Teacher(id, name, availabilities))
+//        case Role.CoAdvisor(External(id, name, _)) => Role.CoAdvisor(External(id, name, availabilities))
+//        case Role.Supervisor(External(id, name, _)) => Role.Supervisor(External(id, name, availabilities))
+//        case _ => role
+//
   def schedulableVivaGen(jury: List[Role]):  Gen[Viva] =
     for
       student <- studentGen
@@ -72,32 +83,64 @@ object ScheduleVivaServiceProperties extends Properties("ScheduleVivaServiceProp
       viva    <- Viva.from(student, title, jury).fold(_ => Gen.fail, Gen.const)
     yield viva
 
+  def mergeResources(resourceList: List[Resource], updatedResources: List[Resource]): List[Resource] =
+    resourceList.map(r => updatedResources.find(_.id == r.id).fold(ifEmpty = r)(updated => updated))
+
   /**
    * Edge cases missing, must be improved.
    */
-  def schedulableVivasGen(resources: List[Resource])(duration: Duration): Gen[List[Viva]] =
+  def schedulableVivasGen(resources: List[Resource])(duration: Duration): Gen[(List[Resource],List[Viva])] =
     for
       n  <- Gen.chooseNum(1, MAX_VIVAS)
       lg = (1 to n).foldLeft(Gen.const((resources, List.empty[Viva])))((gll, _) =>
         for
           (resourcesList, vivaList) <- gll
-          someResources             <- Gen.someOf(resourcesList)
-          jury                      <- rolesGen(someResources.toSet)
           candidateAvailability     <- schedulableAvailabilityGen(duration)
-          juryUpdated               <- Gen.sequence[List[Role], Role](jury.map(j => updateResourceAvailability(j, candidateAvailability, duration)))
-          viva                      <- schedulableVivaGen(juryUpdated)
-        yield (resourcesList, vivaList.::(viva))
+//          nSome                     <- Gen.choose(2, 15).map()
+//          someResources             <- Gen.listOfN(nSome, resourcesList).suchThat(l => )
+//          someResources             <- Gen.choose()
+          someResources             <- Gen.someOf(resourcesList).suchThat(r => getNumOfTeachers(r.toList) >= 2)
+          updatedResources          <- Gen.sequence[List[Resource], Resource](someResources.map(r => updateResourceAvailability(r, candidateAvailability, duration)))
+          jury                      <- rolesGen(updatedResources.toSet)
+//          juryUpdated               <- Gen.sequence[List[Role], Role](jury.map(j => updateResourceAvailability(j, candidateAvailability, duration)))
+          viva                      <- schedulableVivaGen(jury)
+        yield (mergeResources(resourcesList, updatedResources), vivaList.::(viva))
       )
-      lv <- lg.map((_, vivaList) => vivaList)
+      lv <- lg//.map((resourceList, vivaList) => vivaList)
     yield lv
 
   def schedulableAgendaGen: Gen[Agenda] =
     for
       duration  <- durationGen
       resources <- schedulableResourcesGen
-      vivas     <- schedulableVivasGen(resources)(duration)
-    yield Agenda(duration, vivas, resources)
+      (resourcesUpdated, vivas) <- schedulableVivasGen(resources)(duration)
+    yield Agenda(duration, vivas, resourcesUpdated)
 
   property("Schedule agenda") =
     forAll(schedulableAgendaGen):
       a => ScheduleVivaService.scheduleVivaFromAgenda(a).isRight
+
+//  property("SchedulableVivasGen must generate valid vivas") =
+//    val sv = for
+//      duration  <- durationGen
+//      resources <- schedulableResourcesGen
+//      (resourcesUpdated, vivas) <- schedulableVivasGen(resources)(duration)
+//      agenda    = Agenda(duration, vivas , resourcesUpdated)
+//      sv        <- ScheduleVivaService.scheduleVivaFromAgenda(agenda)
+//    yield sv
+//    sv.sample match
+//      case Some(value) => println(value.isRight)
+//      case None =>
+    //    vivas.sample match
+//      case Some(value) => value.headOption match
+//        case Some(value) => println(value)// println(ScheduleVivaService.scheduleVivaFromAgenda().isRight)
+//        case None => println(" no vivas")
+//      case None => println(" no sample")
+//    true
+
+//    forAll(durationGen): d =>
+//      forAll(schedulableResourcesGen): r =>
+//        forAll(schedulableVivasGen(r)(d)): sv =>
+//          println(sv)
+//          true
+
